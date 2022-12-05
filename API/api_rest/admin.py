db91dc554b9622e50015096b711593383ffb45f9
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-04 22:30:29 -0600

    Add Article Model to the admin but with filters

--- API/api_rest/admin.py
+++ API/api_rest/admin.py
@@ -3,4 +3,7 @@ from .models import Article
 
 
 # Register your models here.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title', 'description', 'slug')
+    list_display = ('title', 'description', 'slug')
