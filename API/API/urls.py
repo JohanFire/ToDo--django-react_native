9fb96d5a0827a535b0af42ea722472070d91d5ab
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-04 22:08:18 -0600

    Create "urls.py" in the new app and reference it, way 1

--- API/API/urls.py
+++ API/API/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from api_rest.views import index
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index)
+    path("", include("api_rest.urls")),
 ]
