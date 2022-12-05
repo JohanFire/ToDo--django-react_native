9ffb1f2c8d6dea780ca73b2a0bc63ecd8621b842
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-04 22:16:14 -0600

    Creating Article model and adding the app to settings.py

--- API/api_rest/models.py
+++ API/api_rest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    # Add in thumbnail and author later
+    def __str__(self):
+        return self.title
+
+    # def snippet(self):
+    #     return self.body[:50] + '...'
