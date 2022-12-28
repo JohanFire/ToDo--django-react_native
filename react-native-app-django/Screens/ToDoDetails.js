a635a57b226ec15ca377cf51ec354711bddee8e7
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-27 18:58:05 -0600

    Formatting details

--- react-native-app-django/Screens/ToDoDetails.js
+++ react-native-app-django/Screens/ToDoDetails.js
@@ -5,19 +5,30 @@ export default function ToDoDetails(props) {
     const { id, title, description, slug } = props.route.params.item
 
     return (
-        <View>
-            <Text>ToDoDetails screen</Text>
-            <Text>
-                {title}
-                {description}
-                {slug}
-            </Text>
+        <View style={styles.details}>
+            <Text style={styles.titleDetails}>{title}</Text>
+            <Text style={styles.slugDetails}>#{slug}</Text>
+            <Text style={styles.descriptionDetails}>{description}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-
+    details: {
+        padding: 10,
+        margin: 10,
+    },
+    titleDetails: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    slugDetails: {
+        fontSize: 13,
+        color: 'gray',
+        marginTop: -2,
+    },
+    descriptionDetails: {
+        fontSize: 14,
+        marginTop: 10,
     },
 })
