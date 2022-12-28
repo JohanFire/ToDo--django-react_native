be60b29d8cf00eea3655ba462e26a4e5ed12734a
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-27 19:49:48 -0600

    Adding Scroll so can scroll if screen is full of information

--- react-native-app-django/Screens/ToDoDetails.js
+++ react-native-app-django/Screens/ToDoDetails.js
@@ -1,28 +1,30 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import { Button } from 'react-native-paper'
 
 export default function ToDoDetails(props) {
     const { id, title, description, slug } = props.route.params.item
 
     return (
-        <View style={styles.details}>
-            <Text style={styles.titleDetails}>{title}</Text>
-            <Text style={styles.slugDetails}>#{slug}</Text>
-            <Text style={styles.descriptionDetails}>{description}</Text>
-            <View style={styles.btnStyle}>
-                <Button style={{ backgroundColor: "#1f533e", borderRadius: 4 }}
-                    icon="update"
-                    mode="contained"
-                    onPress={() => console.log("Update button pressed")}
-                >Update</Button>
-                <Button style={{ backgroundColor: "#960d0b", borderRadius: 4 }}
-                    icon="delete"
-                    mode='contained'
-                    onPress={() => console.log("Delete button pressed")}
-                >Delete</Button>
-            </View>
-        </View >
+        <ScrollView>
+            <View style={styles.details}>
+                <Text style={styles.titleDetails}>{title}</Text>
+                <Text style={styles.slugDetails}>#{slug}</Text>
+                <Text style={styles.descriptionDetails}>{description}</Text>
+                <View style={styles.btnStyle}>
+                    <Button style={{ backgroundColor: "#1f533e", borderRadius: 4 }}
+                        icon="update"
+                        mode="contained"
+                        onPress={() => console.log("Update button pressed")}
+                    >Update</Button>
+                    <Button style={{ backgroundColor: "#960d0b", borderRadius: 4 }}
+                        icon="delete"
+                        mode='contained'
+                        onPress={() => console.log("Delete button pressed")}
+                    >Delete</Button>
+                </View>
+            </View >
+        </ScrollView>
     )
 }
 
